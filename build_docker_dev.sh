054e8d794e6b357aed091ce4dec127169a4f4578
OJ LaBoeuf <orangepeelbeef@users.noreply.github.com>
2022-02-03 05:28:45 -0500

    update build scripts to pull ubuntu 20.04

--- build_docker_dev.sh
+++ build_docker_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker pull Ubuntu:18.04 
+docker pull Ubuntu:20.04 
 docker build . -t orangepeelbeef/pia-proxyboxng:dev --no-cache
 docker push orangepeelbeef/pia-proxyboxng:dev
--- build_docker_stable.sh
+++ build_docker_stable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker pull ubuntu:18.04
+docker pull ubuntu:20.04
 docker build . -t orangepeelbeef/pia-proxyboxng:latest -t orangepeelbeef/pia-proxyboxng:stable --no-cache
 docker push orangepeelbeef/pia-proxyboxng:latest
 docker push orangepeelbeef/pia-proxyboxng:stable
